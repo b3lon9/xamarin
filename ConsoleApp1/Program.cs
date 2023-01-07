d59eaed7e05760225091580c0f319f04121915f3
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-07 23:56:08 +0900

    #1 Hello World

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 주석은 이걸 사용하는구나.
+            Console.WriteLine("Hello World ! ");    // Android Studio가 상위 버전이라 하지만, 뭔가 더 친근한 이 느낌은 뭘까
+
+            // 빌드는 Ctrl + Shift + b
+            // 결과확인 : Ctrl + F5
+
+            // F5 만 누른 것은 디버깅을 하는 작업이다
+            // 'using' 네임스페이스를 사용하는 것을 뜻함
+            // 네임스페이스는 Class, Structure, Interface를 그룹화 한 것
+
+            // Console.WriteLine 함수는 System NameSpace으로 인해 사용한 것
+        }
+    }
+}
