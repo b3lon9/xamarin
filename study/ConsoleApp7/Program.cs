e1701f63b15c20a0b2df80424677381e3c643b57
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-10 15:44:37 +0900

    #8 extension method 확장함수
    params -> (this int a, int b)
    사용시 a.classname(b) 식으로 사용이

---
+++ study/ConsoleApp7/Program.cs
@@ -0,0 +1,41 @@
+﻿using Extension;
+using System;
+
+namespace Extension
+{
+    public static class IntExtension
+    {
+        public static int Square(this int width, int height)
+        {
+            return width * height;
+        }
+    }
+}
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        // 분할 클래스partial class : 동일한 클래스를 나누어 작업
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{12.Square(8)}");
+        }
+        //class TestGun     // 에러 발생 ctrl + k, c
+        //{
+        //    public void
+        //}
+        partial class TestGun
+        {
+            public void Fire() { }
+        }
+
+        partial class TestGun
+        {
+            public void Fire2() { }
+        }
+
+
+    }
+}
+// 확장
