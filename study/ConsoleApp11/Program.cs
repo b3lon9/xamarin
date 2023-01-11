9ba9915844728503e156c69d318d05ffdde3d655
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-11 12:19:02 +0900

    #12 event + delegate
    - 내가 볼 땐 더러운 코드다.. 꼬아서 또 꼬은
    - 하지만 현업에서는 다양한 형태로 범용가능한 이벤트를 구현할 때 사용하면 좋을 것 같은데...

---
+++ study/ConsoleApp11/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    delegate void EventHandler(int number, string message);
+    
+    // 방문자 이벤트
+    class Visitor
+    {
+        public event EventHandler EventCall;
+
+        public void VisitorCheck(int VisitorNum)
+        {
+            if (VisitorNum % 3 == 0)
+            {
+                EventCall(VisitorNum, "방문자");
+            }
+        }
+    }
+    
+    class Celebration
+    {
+        public void VisitorEvent(int number , string message)
+        {
+            if (number == 9)
+            {
+                Console.WriteLine("--- ---");
+                Console.WriteLine("{0}번째 {1}로 이벤트 당첨..?", number, message);
+            }
+            else
+            {
+                Console.WriteLine("방문자 이벤트 발생!");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Function()
+        {
+            // Function
+            Console.WriteLine("Function() ...");
+        }
+
+        static void Function2()
+        {
+            Console.WriteLine("Function2()...");
+        }
+
+        delegate void Worker();
+        static void Main(string[] args)
+        {
+            Worker w1 = new Worker(Function);
+            w1();
+
+            w1 += new Worker(Function2);
+            w1();
+
+            // 이벤트를 받을 객체 인스턴스
+            Celebration celub = new Celebration();
+
+            // 이벤트 발생 인스턴스
+            Visitor vis = new Visitor();
+
+            vis.EventCall += new EventHandler(celub.VisitorEvent);
+
+            for (int i=0; i<10; i++)
+            {
+                vis.VisitorCheck(i);
+            }
+        }
+    }
+}
