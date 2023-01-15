83d8505fffb92e7ad6d1563f1d6ba190871ffdc4
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-15 21:40:01 +0900

    xamarin #2 button event
    - add button event
    - debug output logcat display

---
+++ app/xa002/xa002/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xa002
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            // debug 찍어보기
+            System.Diagnostics.Debug.WriteLine("버튼이 눌러졌음");
+        }
+    }
+}
