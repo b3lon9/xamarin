7f8d7bb72b52cbdfc79491f988bb8126216953da
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-17 15:14:22 +0900

    gcamp - #3 absolutelayout
    LayoutBounds = 위치 / 크기
    LayoutFlags = 위치, 크기 proportional 지정

---
+++ gcamp/absolutelayout/lec07/AbsoluteCSEx.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lec07
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AbsoluteCSEx : ContentPage
+    {
+        public AbsoluteCSEx()
+        {
+            // InitializeComponent();
+            // xaml ui를 사용하지 않겠다는 의미인 것 같다
+            AbsoluteLayout absoluteLayout = new AbsoluteLayout();
+
+            Label label01 = new Label();
+
+            label01.Text = "Red";
+            label01.BackgroundColor = new Color(255, 0, 0);
+
+            // bindable
+            AbsoluteLayout.SetLayoutBounds(label01, new Rectangle(100, 200, 100, 100));
+            AbsoluteLayout.SetLayoutFlags(label01, AbsoluteLayoutFlags.None);
+
+            absoluteLayout.Children.Add(label01);
+
+
+            Label label02 = new Label();
+
+            // 마지막 absoluteLayout을 어디다 붙여?
+            // contentPage에 붙인다
+            this.Content = absoluteLayout;
+        }
+    }
+}
---
+++ gcamp/absolutelayout/lec07/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lec07
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AbsoluteCSEx();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
