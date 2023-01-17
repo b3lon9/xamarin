3e23bf2aa82ee581efe6d2dd9d0f82d321926574
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-17 11:01:52 +0900

    gcamp - lec # 01
    - 개발환경구성 및 디버깅

---
+++ lec01/lec01/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lec01
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // 처음 시작
+            Debug.WriteLine("Debug --> OnStart");
+        }
+
+        protected override void OnSleep()
+        {
+            // 홈 버튼 누른 경우 : 백그라운드로 간 경우
+            Debug.WriteLine("Debug --> OnSleep");
+        }
+
+        protected override void OnResume()
+        {
+            // 다시 앱에 진입한 경우
+            Debug.WriteLine("Debug ->> OnResume");
+        }
+    }
+}
