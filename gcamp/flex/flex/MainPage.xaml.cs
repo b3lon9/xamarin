bb72d988cd17357d24fd078189d28bb75e5c0fdc
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-17 17:53:46 +0900

    gcamp - #5 flexlayout
    html css display = "flex" 랑 동일한 문구이다
    
    Direction = " Column:세로 / Row:가로 "
    AlignItems = " Center, Start, Stretch "  > 자식 뷰들의 gravity 설정
    JustifyContent = " SpaceEvenly, SoaceBetween'" > 자식 뷰들의 관계 설정

---
+++ gcamp/flex/flex/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace flex
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+
+            //StackLayout stackLayout = new StackLayout();
+            //Label label01 = new Label();
+            //label01.Text = "Hello Xamarin!";
+
+            //stackLayout.Children.Add(label01);
+
+            //this.Content = stackLayout;
+
+            InitializeComponent();
+
+        }
+    }
+}
