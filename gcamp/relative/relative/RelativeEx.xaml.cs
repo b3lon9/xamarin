5c212ea9224d06944878b0ce12bfcf6b7110388f
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-17 16:49:15 +0900

    gcamp - #4 relativelayout
    - 기본적인 것은 RelativeToParent인 경우 부모 뷰의 크기를 본인 입맛에 맞게 적용
    - RelativeToView  => ElementName=x.Name으로 만든 element를 참조해서 child view의 크기를 따라서 입맛에 맞게 적용

---
+++ gcamp/relative/relative/RelativeEx.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace relative
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RelativeEx : ContentPage
+    {
+        public RelativeEx()
+        {
+            //InitializeComponent();    // initalizeComponent가 선언되지 않으면 xaml파일이 그려지지 않음
+
+            RelativeLayout relativeLayout = new RelativeLayout();
+
+            Label label01 = new Label();
+            label01.Text = "Wrap";
+            label01.BackgroundColor = Color.FromHex("cccccc");
+            relativeLayout.Children.Add(
+                label01,
+                Constraint.RelativeToParent((parent) => { return parent.Width * 0; }),
+                Constraint.RelativeToParent((parent) => { return parent.Height * 0; }),
+                Constraint.RelativeToParent((parent) => { return parent.Width * 1; }),
+                Constraint.RelativeToParent((parent) => { return parent.Height * .8; })
+                );
+
+            Label label02 = new Label ();
+            label02.Text = "Wrap";
+            label02.BackgroundColor = Color.FromHex("ff0000");
+            relativeLayout.Children.Add(
+                label02,
+                Constraint.RelativeToParent(parent => { return parent.Width * .2; }),
+                Constraint.RelativeToParent(parent => { return parent.Height * .5; }),
+                Constraint.RelativeToView(label01, (parent, child) => { return child.Width * .5; }),
+                Constraint.RelativeToView(label01, (parent, child) => { return child.Height * .5; })
+                );
+
+            this.Content = relativeLayout;
+        }
+    }
+}
