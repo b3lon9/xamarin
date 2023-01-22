4777bf4ad4103de2b593c6b37a6b94c212dcda65
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-22 14:02:41 +0900

    gcamp #23 MVVM pattern
    - 실시간 시간 갱신

---
+++ gcamp/MVVM/MVVM/ShowCurrentTimeEx02.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVM
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShowCurrentTimeEx02 : ContentPage
+    {
+        public ShowCurrentTimeEx02()
+        {
+            InitializeComponent();
+
+            // Timer만들기 : 1초마다 갱신함
+            Device.StartTimer(TimeSpan.FromSeconds(1), timer);
+
+            // 데이터가 변경은 되었지만, View와 함수가 서로 물고 있기 때문에
+            // 불편함
+        }
+        
+        private bool timer()
+        {
+            label01.Text = $"{DateTime.Now}";
+            label02.Text = $"The year is {DateTime.Now.Year}";
+            label03.Text = $"The month is {DateTime.Now.Month}";
+            label04.Text = $"The day is {DateTime.Now.Day}";
+            label05.Text = $"The time is {DateTime.Now.TimeOfDay}";
+
+            return true;
+        }
+    }
+}
