2c262214b255abe270ad4bed46459f9c9a8111ab
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-22 11:31:36 +0900

    gcamp - #22 암시,동적 스타일
    - implicit style은 x:Key를 명시하지 않은 것
    - 동적인 것은 속성의 스타일을 다른 것을 바꾸는 것 {DynamicResources searchBarStyle}

---
+++ gcamp/Pjt22/Pjt22/StyleDynamicEx.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pjt22
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StyleDynamicEx : ContentPage
+    {
+        bool isOriginalStyle = true;
+        public StyleDynamicEx()
+        {
+
+            InitializeComponent();
+            // Dictionary라고 생각하면 됨
+            // 오른쪽에 있는 값을 searchBarStyle이라는 것에 할당을 해주는 것
+            Resources["searchBarStyle"] = Resources["blueSearchBarStyle"];
+        }
+
+        private void onButtonClicked(object sender, EventArgs e)
+        {
+            var id = ((Button)sender).Id;
+
+            if (isOriginalStyle == true)
+            {
+                Resources["searchBarStyle"] = Resources["greenSearchBarStyle"];
+                isOriginalStyle = false;
+            }
+            else
+            {
+                Resources["searchBarStyle"] = Resources["blueSearchBarStyle"];
+                isOriginalStyle = true; 
+            }
+        }
+    }
+}
