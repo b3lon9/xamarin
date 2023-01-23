b03ee13d6a4fa4fdfc184cbcc1072a21b0762dea
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-23 18:54:45 +0900

    gcamp #24 INotifyPropertyChanged
    - VIewModel에서 데이터 변화를 BroadCast에 알리고,
    - xaml에서 해당 변화를 감지하는 Binding을 함

---
+++ gcamp/MVVM/MVVM/CurrentTimeViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MVVM
+{
+    // Property에 변화가 생기면 알려주는 INotifyPropertyChanged
+    public class CurrentTimeViewModel : INotifyPropertyChanged
+    {
+        DateTime dateTime;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public CurrentTimeViewModel()
+        {
+            this.DateTime = DateTime.Now;
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                // 1초마다 새로운 날짜 갱신
+                this.DateTime = DateTime.Now;
+                return true;
+            });
+        }
+
+        public DateTime DateTime 
+        { 
+            get 
+            { 
+                return dateTime; 
+            }
+            set 
+            {
+                if (dateTime != value)
+                {
+                    dateTime = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        // 갱신되었다고 알려주는 부분
+                        PropertyChanged(this, new PropertyChangedEventArgs("DateTime"));
+
+                        // Notify를 듣는 것은 xaml에서 설정해주어야 함
+                    }
+                }
+            } 
+        }
+
+    }
+}
