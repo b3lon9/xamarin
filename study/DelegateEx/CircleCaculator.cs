99e6d1882ba9c4980c3d06cc3a1e5df318c70553
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-24 08:33:34 +0900

    gcamp #cs delegate
    delegate 기본사용법

---
+++ study/DelegateEx/CircleCaculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateEx
+{
+    // 참조하고 하는 메서드 형태와 동일하게 형태를 맞춰줌
+    delegate double CircleDelegate(double r);
+    class CircleCaculator
+    {
+        double circleRatio = 3.14d;
+        
+        public CircleCaculator()
+        {
+            Console.WriteLine("===CircleCaculator");
+        }
+
+        public double GetArea(double r)
+        {
+            // 넓이, 반지름 * 반지름
+            return r * r * circleRatio;
+        }
+
+        public double GetLength(double r)
+        {
+            // 둘레, 반지름 * 원주율
+            return r * 2 * circleRatio;
+        }
+    }
+}
---
+++ study/DelegateEx/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace DelegateEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            // 객체생성
+            CircleCaculator circleCalculator = new CircleCaculator();
+
+            // delegate 생성
+            CircleDelegate circleDelegate = new CircleDelegate(circleCalculator.GetArea);
+            double circleArea = circleDelegate(10);     // GetArea함수로 계산 됨
+
+            Console.WriteLine($"curcleArea : {circleArea}");
+
+            circleDelegate = new CircleDelegate(circleCalculator.GetLength);
+            double circleLength = Math.Round(circleDelegate(10), 2);
+            Console.WriteLine($"circleLength : {circleLength}");
+        }
+    }
+}
