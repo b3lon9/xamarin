ea981711dfbc95ef8dbe4ba7c3758d3a8ba35635
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-26 13:29:28 +0900

    gcamp #27 DataConverter
    - Binding개념 재이해
    - Entry에서 입력된 텍스트 길이값(int)를 Converter를 통해 (bool)로 돌려받아 Button IsEnabled에서 활성/비활성을 동작하도록 구성함

---
+++ study/DataConverter/DataConverter/DataConverterEx01.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DataConverter
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DataConverterEx01 : ContentPage
+    {
+        public DataConverterEx01()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ study/DataConverter/DataConverter/IntToBoolConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DataConverter
+{
+    public class IntToBoolConverter : IValueConverter
+    {
+        // Text:string의 값을 보고 Button:bool(IsEnabled)값으로 바뀌도록
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // 소스에서 들어오는 값이 value
+            // 텍스트의 길이가 들어올 것이기 때문에 (int)value
+            // 텍스트의 길이가 1이상이면 'true'가 반환되도록
+            // return은 target이 받음
+            return (int)value != 0;
+        }
+
+
+        // 그냥 Convert의 반대개념
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // true면 1 반환
+            // false면 0 반환
+            return (bool)value ? 1 : 0;
+        }
+    }
+}
