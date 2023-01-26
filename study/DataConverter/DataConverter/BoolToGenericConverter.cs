e4f5e2deca40b2a6f4edafe77bfec5ceab0f3777
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-26 14:09:05 +0900

    gcamp #27 - Converter DarkMode
    - xaml을 계층적구조로 이용하여 설계함

---
+++ study/DataConverter/DataConverter/BoolToGenericConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DataConverter
+{
+    // object
+    public class BoolToGenericConverter<T> : IValueConverter
+    {
+        // T뭔지 몰라
+        // 사용자가 무엇을 원하는지 모르지만 Generic : T로 넘겨주어 확인
+        public T TrueObject { get;  set; }
+        public T FalseObject { get;  set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool) value ? TrueObject : FalseObject;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((T)value).Equals(TrueObject);
+        }
+    }
+}
