ca462d825d8c0dd0eaed360d772af621f5b5394e
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-27 09:35:27 +0900

    C# 14강 배열
    일반 배열

---
+++ study/ArrayPjt/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO.Pipes;
+
+Console.WriteLine("Hello, World!");
+
+
+
+string[] names = new string[5] {"정우땡", "홍상땡", "송수땡", "황승땡", "이대땡"}; // 배열 선언
+int len = names.Length;
+Console.WriteLine("names len : {0}", len);
+
+int index = Array.IndexOf(names, "송수땡");
+Console.WriteLine("IndexOf 송수땡 : {0}", index);
+
+// 틀려보자
+int indexEr = Array.IndexOf(names, "에러");
+Console.WriteLine("IndexOf Er : " + indexEr);
+
+Array.Reverse(names);
+foreach(string name in names)
+{
+    Console.Write($"{name} ");
+}
+
+// Clear
+Array.Clear(names, 0, names.Length-2);
+
+foreach(string name in names) Console.Write($"clear name : {name}");
