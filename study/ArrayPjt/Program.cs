573dd235b01a94fab382ed7bf0a74490f26f8658
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-27 09:42:14 +0900

    c# 14강 배열 객체 선언
    - 새로운 클랙스를 선언하고 해당 클래스를 사용하는 배열들을 사용해보는 예제

--- study/ArrayPjt/Program.cs
+++ study/ArrayPjt/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ArrayPjt;
 using System.IO.Pipes;
 
+/*
 Console.WriteLine("Hello, World!");
-
-
 
 string[] names = new string[5] {"정우땡", "홍상땡", "송수땡", "황승땡", "이대땡"}; // 배열 선언
 int len = names.Length;
@@ -26,3 +26,19 @@ foreach(string name in names)
 Array.Clear(names, 0, names.Length-2);
 
 foreach(string name in names) Console.Write($"clear name : {name}");
+*/
+
+Student[] students = new Student[5];
+students[0] = new Student("박찬호", 20, "baseball", 2);
+students[1] = new Student("손흥민", 19, "soccer", 1);
+students[2] = new Student("김연경", 21, "Volleyball", 3);
+students[3] = new Student("서장훈", 22, "basketball", 4);
+students[4] = new Student("박태환", 22, "swimming", 4);
+
+Console.WriteLine($"Students Size : {students.Length}");
+students[0].PrintStudentInfo();
+
+foreach(Student student in students)
+{
+    student.PrintStudentInfo();
+}
---
+++ study/ArrayPjt/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPjt
+{
+    class Student
+    {
+        // 데이터는 손쉽게 접근하지 않도록 수정
+        private string  Name;
+        private int     Age;
+        private string  Major;
+        private int     Grade;
+
+        public Student()
+        {
+            Console.WriteLine("[Student Constructor]");
+        }
+
+        public Student(string name, int age, string major, int grade)
+        {
+            this.Name   = name;
+            this.Age    = age;
+            this.Major  = major;
+            this.Grade  = grade;
+        }
+
+        public void PrintStudentInfo()
+        {
+            Console.WriteLine($"Name : {Name}");
+            Console.WriteLine($"Age : {Age}");
+            Console.WriteLine($"Name : {Name}");
+            Console.WriteLine($"Name : {Name}");
+        }
+
+        // Getter&Setter
+        public string GetName(string masterId)
+        {
+            string name = "";
+            if (masterId == "admin")
+            {
+                name = this.Name;
+            }
+            else
+            {
+                name = "You Don't have access.";
+            }
+
+            return name;
+        }
+
+        public string GetMajor(string masterId)
+        {
+            string major = "";
+            if (masterId == "admin")
+            {
+                major = this.Major;
+            }
+            else
+            {
+                major = "You don't have Access";
+            }
+            return major;
+        }
+    }
+}
