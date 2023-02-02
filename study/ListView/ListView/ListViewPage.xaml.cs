0e1b9c09818d707e98076a1d3820c515996fce2f
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-02-02 23:44:36 +0900

    gcamp #28 ListView Select
    - Selected vs Tapped
    selected는 새로운 아이템을 클릭했을 경우 이벤트 발생
    tapped는 언제든지 터치하면 이벤트 발생

--- study/ListView/ListView/ListViewPage.xaml.cs
+++ study/ListView/ListView/ListViewPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,12 @@ namespace ListView
     {
         public ListViewPage()
         {
-            // InitializeComponent();
+            InitializeComponent();
+            // init();
+        }
+
+        private void init()
+        {
             this.Padding = 15;
 
             StackLayout stackLayout = new StackLayout();
@@ -54,9 +60,23 @@ namespace ListView
 
             Xamarin.Forms.ListView listView = new Xamarin.Forms.ListView();
             listView.ItemsSource = strs;    //Property
-            
+
             stackLayout.Children.Add(listView);
             this.Content = stackLayout;
+        }
+
+        // 무슨 상황이던 터치를 하면 이벤트 발생
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            string str = (string)e.Item;
+            System.Diagnostics.Debug.WriteLine($"e.Item : {str}");
+        }
+
+        // 선택되지 않은 아이템을 클릭하면 이벤트 발생(새롭게)
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            string str = (string)e.SelectedItem;
+            System.Diagnostics.Debug.WriteLine($"e.Select :{str}");
         }
     }
 }
