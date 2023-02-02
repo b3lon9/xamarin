497df3d43b5ef68ba66ffae36e2e73a1e4171e46
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-02-02 23:38:31 +0900

    gcamp #28 ListView
    - ListView structure

---
+++ study/ListView/ListView/ListViewPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewPage : ContentPage
+    {
+        public ListViewPage()
+        {
+            // InitializeComponent();
+            this.Padding = 15;
+
+            StackLayout stackLayout = new StackLayout();
+
+            string[] strs = new string[]
+            {
+                // 바로 초기화
+                "c",
+                "c++",
+                "java",
+                "android",
+                "xamarin",
+                "take",
+                "spring",
+                "python",
+                "djangoframwork",
+                "apache","c",
+                "c++",
+                "java",
+                "android",
+                "xamarin",
+                "take",
+                "spring",
+                "python",
+                "djangoframwork",
+                "apache","c",
+                "c++",
+                "java",
+                "android",
+                "xamarin",
+                "take",
+                "spring",
+                "python",
+                "djangoframwork",
+                "apache",
+            };
+
+            Xamarin.Forms.ListView listView = new Xamarin.Forms.ListView();
+            listView.ItemsSource = strs;    //Property
+            
+            stackLayout.Children.Add(listView);
+            this.Content = stackLayout;
+        }
+    }
+}
