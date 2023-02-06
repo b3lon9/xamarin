f4edea2447d35798ce3f39488d534caf3ca4c5e0
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-02-06 10:19:45 +0900

    TEST #1 로그인 페이지 이동
    - MainPage에서 로그인 페이지로 이동이 되는지 확인
    
    > 목표는 SplashImage이며, Splash이미지 또는 MainPage내에서 동기화 처리가 필요한 부분을 처리한 뒤에 로그인 페이지로 이동하도록 하는 것이 목표

---
+++ neander_cheonha_app/neander_cheonha_app/view/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using neander_cheonha_app.view;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace neander_cheonha_app
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangePage()
+        {
+            System.Diagnostics.Debug.WriteLine($"[debug:{this}] ChangePage()");
+            Application.Current.MainPage = new LoginPage();
+        }
+
+        private void BtnLoginPage_Clicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine($"[debug:{this}] BtnLoginPage_Clicked()");
+            Application.Current.MainPage = new LoginPage();
+        }
+    }
+}
