5bd47f9986df591a6ab9760cea4cccb841bf1119
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-03-06 14:42:21 +0900

    #TEST : 토글색상 버튼 적용완료

--- study/Test/Test/Views/AboutPage.xaml.cs
+++ study/Test/Test/Views/AboutPage.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.ComponentModel;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace Test.Views
 {
@@ -10,6 +8,49 @@ namespace Test.Views
         public AboutPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var id = ((Button)sender).Id;
+
+            // 색상 통일 시켜줄 경우 필요한 로직
+            // var btn = id == PlanButton.Id ? PlanButton : (id == IngButton.Id? IngButton : CompleteButton);
+
+            if (PlanButton.Id == id)
+            {
+                if (PlanButton.BackgroundColor == Color.LightSkyBlue)
+                {
+                    ((Button)sender).BackgroundColor = Color.FromHex("#ececec");
+                }
+                else
+                {
+                    PlanButton.BackgroundColor = Color.LightSkyBlue;
+                }
+            }
+            else if (IngButton.Id == id)
+            {
+                if (IngButton.BackgroundColor == Color.LightSkyBlue)
+                {
+                    ((Button)sender).BackgroundColor = Color.FromHex("#eed569");
+                }
+                else
+                {
+                    IngButton.BackgroundColor = Color.LightSkyBlue;
+                }
+            }
+            else if (CompleteButton.Id == id)
+            {
+                if (CompleteButton.BackgroundColor == Color.LightSkyBlue)
+                {
+                    ((Button)sender).BackgroundColor = Color.FromHex("#82d280");
+                }
+                else
+                {
+                    CompleteButton.BackgroundColor = Color.LightSkyBlue;
+                }
+            }
+            
         }
     }
 }
